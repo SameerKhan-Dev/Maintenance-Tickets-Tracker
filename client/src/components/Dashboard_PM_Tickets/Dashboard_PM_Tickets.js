05ca7f016ecc7021dba64c3cb1a401fccf500177
zahra-hm <zahra520@hotmail.com>
2021-02-17 20:19:54 +0000

    add side nav bar; top nav bar in progress

--- client/src/components/Dashboard_PM_Tickets/Dashboard_PM_Tickets.js
+++ client/src/components/Dashboard_PM_Tickets/Dashboard_PM_Tickets.js
@@ -1,2 +1,28 @@
-import Employee_List from "./Employee_List";
+import React, {useState, Component} from 'react';
+import {useEffect} from 'react';
+import "../Application.scss";
 
+import Employee_List_PM from "./Employee_List_PM";
+import Ticket_List_PM from "./Ticket_List_PM";
+// import Side_NavBar_PM_Stats from "/Users/zahrahm/lighthouse/final-project/Maintenance-Tickets-Tracker/client/src/components/Dashboard_PM_Stats/Side_NavBar_PM_Stats.js";
+import Side_NavBar_PM_Tickets from "./Side_NavBar_PM_Tickets"
+import Top_NavBar_PM_Tickets from "./Top_Nav_Bar_PM_Tickets";
+
+
+export default function Dashboard_PM_Tickets(props) {
+
+    return (
+      <>
+        <div>
+          <Side_NavBar_PM_Tickets/>
+        </div>
+        <div>
+          <Employee_List_PM />
+          <Ticket_List_PM />
+        </div>
+        <div>
+        <Top_NavBar_PM_Tickets/>
+      </div>
+      </>
+    );
+}
---
+++ client/src/components/Dashboard_PM_Tickets/Top_Nav_Bar_PM_Tickets.js
@@ -0,0 +1,43 @@
+
+import React, {useState, Component} from 'react';
+import {useEffect} from 'react';
+
+import "./Top_Nav_Bar_PM_Tickets.scss";
+// import "./map.scss";
+// //import Ticket_List_Item from  "./Ticket_List_Item"
+// //import Ticket_List from  "./Ticket_List"
+// import PropertyList from "./PropertyList";
+// import Map_SideBar from "./property_map";
+
+
+export default function Top_NavBar_PM_Tickets(props) {
+  return (
+    <>
+    {/* <Navbar>
+  <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+  <Navbar.Toggle />
+  <Navbar.Collapse className="justify-content-end">
+    <Navbar.Text>
+      Signed in as: <a href="#login">Mark Otto</a>
+    </Navbar.Text>
+  </Navbar.Collapse>
+</Navbar> */}
+    </>
+  )
+
+//   return (
+//     <div>
+//       <ul id="nav">
+//         <div classname='nav-front'>
+//           <li><a href="#">M Trackers</a></li>
+//         </div>
+//         <div classname='nav-end'>
+//           <li><a href="#">Logged in as [email]</a></li>
+//           <li><a href="#">Logout</a></li>
+//         </div>
+//       </ul>
+//     </div>
+// );
+}
+
+
