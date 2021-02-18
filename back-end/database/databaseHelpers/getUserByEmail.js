275398438ff69999ced364c9bf13273b1b6b7b68
Amphakarn <pisuthigomol@yahoo.com>
2021-02-18 05:32:24 +0000

    Added query getUserByEmail for login user credential

--- back-end/database/databaseHelpers/getUserByEmail.js
+++ back-end/database/databaseHelpers/getUserByEmail.js
@@ -1,1 +1,24 @@
-const pool = require('./database');
+const db = require("../database");
+
+const getUserByEmail = function (email) {
+  return db
+    .query(
+      `
+      SELECT *, roles.role FROM users
+      JOIN roles ON role_id = roles.id
+      WHERE users.email = $1;`,
+      [email]
+    )
+    .then((res) => {
+      if (res.rows) {
+        console.log("res.rows is, ", res.rows);
+        return res.rows;
+      } else {
+        console.log("null returned");
+        return null;
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = getUserByEmail;
