446e8aca8eb9539b04891bde6eb69312aec235af
Amphakarn <pisuthigomol@yahoo.com>
2021-02-18 23:58:34 +0000

    Updated queries to specify table name after select

--- back-end/database/databaseHelpers/getAllPropertiesByPM_Id.js
+++ back-end/database/databaseHelpers/getAllPropertiesByPM_Id.js
@@ -1,24 +1,23 @@
-const db = require('../database');
+const db = require("../database");
 
 const getAllPropertiesByPM_Id = function (PM_Id) {
+  return db
+    .query(
+      `
+      SELECT properties.* FROM properties
+      WHERE property_manager_id = $1;`,
+      [PM_Id]
+    )
+    .then((res) => {
+      if (res.rows) {
+        // console.log("res.rows is, ", res.rows);
+        return res.rows;
+      } else {
+        console.log("null returned");
+        return null;
+      }
+    })
+    .catch((err) => console.log(err));
+};
 
-    return db.query(`
-        SELECT * FROM properties
-        WHERE property_manager_id = $1;`, [PM_Id])
-      .then(res => {
-        if (res.rows) {
-  
-          // console.log("res.rows is, ", res.rows);
-          return res.rows;
-  
-        } else {
-  
-          console.log("null returned");
-          return null;
-        }
-      })
-      .catch(err => console.log(err));
-  
-  }
-
-  module.exports = getAllPropertiesByPM_Id;
+module.exports = getAllPropertiesByPM_Id;
--- back-end/database/databaseHelpers/getAllTicketsByPm_Id.js
+++ back-end/database/databaseHelpers/getAllTicketsByPm_Id.js
@@ -1,27 +1,24 @@
-const db = require('../database');
+const db = require("../database");
 
 const getAllTicketsByPm_Id = function (pm_id) {
-
-    return db.query(`
-      SELECT * FROM tickets
+  return db
+    .query(
+      `
+      SELECT tickets.* FROM tickets
       JOIN properties ON tickets.property_id = properties.id
-      WHERE property_manager_id = $1;`
-      , [pm_id])
-      .then(res => {
-        if (res.rows) {
-  
-          // console.log("res.rows is, ", res.rows);
-          return res.rows;
-  
-        } else {
-            
-          console.log("null returned");
-          return null;
-        }
-      })
-      .catch(err => console.log(err));
-  
-  }
+      WHERE property_manager_id = $1;`,
+      [pm_id]
+    )
+    .then((res) => {
+      if (res.rows) {
+        // console.log("res.rows is, ", res.rows);
+        return res.rows;
+      } else {
+        console.log("null returned");
+        return null;
+      }
+    })
+    .catch((err) => console.log(err));
+};
 
-  module.exports = getAllTicketsByPm_Id;
-  
+module.exports = getAllTicketsByPm_Id;
