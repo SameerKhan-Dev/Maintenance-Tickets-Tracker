788655e0e44dff48b9f0f11a581f5fe9cfb9d7d6
SameerKhan-Dev <sameerk_786@hotmail.com>
2021-02-19 20:51:06 +0000

    completed implementation of assigning employee to ticket

--- client/src/components/Dashboard_PM_Tickets/Dashboard_PM_Tickets.js
+++ client/src/components/Dashboard_PM_Tickets/Dashboard_PM_Tickets.js
@@ -4,15 +4,23 @@ import "../Application.scss";
 import axios from "axios";
 import Employee_List_PM from "./Employee_List_PM";
 import Ticket_List_PM_Pending from "./Ticket_List_PM_Pending";
+import specificTicket_Modal from "./specificTicket_Modal";
+import Ticket_List_PM_In_Progress from "./Ticket_List_PM_In_Progress";
 // import Side_NavBar_PM_Stats from "/Users/zahrahm/lighthouse/final-project/Maintenance-Tickets-Tracker/client/src/components/Dashboard_PM_Stats/Side_NavBar_PM_Stats.js";
 import Side_NavBar_PM_Tickets from "./Side_NavBar_PM_Tickets"
 import Top_NavBar_PM_Tickets from "./Top_Nav_Bar_PM_Tickets";
 import "./Dashboard_PM_Tickets.scss";
-
-
+import Button from 'react-bootstrap/Button';
+import Toast from 'react-bootstrap/Toast';
+import ToastHeader from 'react-bootstrap/ToastHeader';
+import ToastBody from 'react-bootstrap/ToastBody';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
+import MyVerticallyCenteredModal from './specificTicket_Modal';
 
 export default function Dashboard_PM_Tickets(props) {
 
+  const [modalShow, setModalShow] = React.useState(false);
   // When user login is setup, extract user_id using cookies
   // temporarily we are going to use user_id as 1 (i.e pm_id for this page)
   const  tempPM_Id = 3
@@ -26,11 +34,42 @@ export default function Dashboard_PM_Tickets(props) {
         totalUnsolved: 0,
         pending: 0,
         in_Progress: 0
-      },
-      ticketsPending: [],
-      ticketsInProgress: []
+      }
     }
   );
+  // add tickete
+
+ 
+
+  const assignEmployeeToTicket = function (ticket_id, employee_id){
+    console.log("inside assignEmployeeToTicket!");
+    console.log("ticket_id is: ", ticket_id);
+    console.log("employee_id is:", employee_id);
+      return axios
+        .put(`/tickets/assignEmployee`, {
+          ticket_id: ticket_id,
+          employee_id: employee_id
+        })
+        .then((response) => {
+          
+          let ticketsArray = [...state_PM_Tickets.tickets];
+          
+          console.log("RESPONSE: ", response.data);
+          for (let ticket of ticketsArray){
+            if (ticket.id === ticket_id) {
+              
+              ticket.ticket_status_id = 2;
+              ticket.employee_id = employee_id;
+              //setState_PM_Tickets(prev => {...prev, prev.ticket})
+            }
+          }
+          setState_PM_Tickets(prev => ({...prev, tickets: ticketsArray}));
+          
+
+
+
+        });
+  }
 
   const constructTicketsData = function (propertiesArray, ticketsArray) {
     let ticketsOrganizedByProperty = []; // our result to be populated and sent out from this helper function.
@@ -104,6 +143,7 @@ export default function Dashboard_PM_Tickets(props) {
     }
      return ticketsOrganizedByProperty;
   } 
+  let ticketsOrganizedByProperty = constructTicketsData(state_PM_Tickets.properties, state_PM_Tickets.tickets);
 
   // const obtainStats for Specific Property:
   // input is all tickets and stats sorted by property
@@ -147,14 +187,86 @@ export default function Dashboard_PM_Tickets(props) {
     // console.log("propertyObject is : ", propertyObject);
 
     }
-
-    setState_PM_Tickets(prev => ({...prev, selectedProperty: property_id, specificStats: obtainStats(state_PM_Tickets.ticketsOrganizedByProperty, property_id), ticketsPending: pendingTicketsArray  , ticketsInProgress:inProgressTicketsArray}));
+    
+    setState_PM_Tickets(prev => ({...prev, selectedProperty: property_id}));
     
   }
 
+  const setTicketsPending = function (){
+
+    const ticketsPending = [];
+    const property_id = state_PM_Tickets.selectedProperty;
+
+    for (let propertyObject of ticketsOrganizedByProperty) {
+      //console.log("propertyObject.ticketsArray is: ");
+      //console.log(propertyObject.ticketsArray);
+      
+      if (propertyObject.property_id === property_id) {
+        //console.log("propertyObject.ticketsArray is: ");
+        //console.log(propertyObject.ticketsArray);
+        
+        for (let ticket of propertyObject.ticketsArray) {
+          //console.log("ticket is: ", ticket);
+          //console.log(ticket);
+          if(ticket.ticket_status_id === 1){
+            ticketsPending.push(ticket);
+          }
+        }
+      
+        
+      }
+      
+    // console.log("propertyObject is : ", propertyObject);
+
+    }
+    return ticketsPending;
+
+  }
+
+  const setTicketsInProgress = function (){
+
+    const ticketsInProgress = [];
+    const property_id = state_PM_Tickets.selectedProperty;
+
+    for (let propertyObject of ticketsOrganizedByProperty) {
+      //console.log("propertyObject.ticketsArray is: ");
+      //console.log(propertyObject.ticketsArray);
+      
+      if (propertyObject.property_id === property_id) {
+        //console.log("propertyObject.ticketsArray is: ");
+        //console.log(propertyObject.ticketsArray);
+        
+        for (let ticket of propertyObject.ticketsArray) {
+          //console.log("ticket is: ", ticket);
+          //console.log(ticket);
+          if(ticket.ticket_status_id === 2){
+            ticketsInProgress.push(ticket);
+          }
+        }
+      
+        
+      }
+      
+    // console.log("propertyObject is : ", propertyObject);
+
+    }
+    return ticketsInProgress;
+
+  }
+
+
+
+
+
+  const ticketsPending = setTicketsPending();
+  const ticketsInProgress = setTicketsInProgress();
   console.log("Current selected property is: ", state_PM_Tickets.selectedProperty);
   console.log("Current ticketsOrganizedByProperty is: ", state_PM_Tickets.ticketsOrganizedByProperty);
   console.log("Pending Tickets are: ", state_PM_Tickets.ticketsPending);
+
+ 
+  let specificStats = obtainStats(ticketsOrganizedByProperty, state_PM_Tickets.selectedProperty);
+
 
   useEffect(() => {
 
@@ -170,17 +282,17 @@ export default function Dashboard_PM_Tickets(props) {
       // Update local state with data from API.
       
       setState_PM_Tickets(prev => ({...prev, properties: propertiesData, tickets: ticketsData, ticketsOrganizedByProperty: constructTicketsData(propertiesData, ticketsData)}));
-      setState_PM_Tickets(prev => ({...prev, specificStats: obtainStats(prev.ticketsOrganizedByProperty, prev.selectedProperty)}));
+      //setState_PM_Tickets(prev => ({...prev, specificStats: obtainStats(prev.ticketsOrganizedByProperty, prev.selectedProperty)}));
+      
       // need to sort our tickets data into a structure like above using a helper function
       //constructTicketsData(propertiesData, ticketsData);
-      
+      //ticketsPending= setTicketsPending();
     });
 
   },[]);
 
     return (
-        <>
-       
+        <> 
         <div>
           <Side_NavBar_PM_Tickets
             selectProperty = {selectProperty}
@@ -189,10 +301,27 @@ export default function Dashboard_PM_Tickets(props) {
         </div>
         <div className= "dashboard-interface">
           <Top_NavBar_PM_Tickets/>
+          <>
+            <Button variant="success">Total: {specificStats.totalUnsolved}</Button>{' '}
+            <Button variant="warning">Pending: {specificStats.pending}</Button>{' '}
+            <Button variant="danger">In-Progress: {specificStats.in_Progress}</Button>
+          </>
+          <h1>     
+            <Badge variant="secondary">Pending Tickets</Badge>
+          </h1>
+
           <Ticket_List_PM_Pending
-            ticketsPending= {state_PM_Tickets.ticketsPending}
-            ticketsInProgress = {state_PM_Tickets.ticketsInProgress}
+            ticketsPending= {ticketsPending}
             selectedProperty= {state_PM_Tickets.selectedProperty}
+            assignEmployeeToTicket = {assignEmployeeToTicket}
+          />
+          <h1>     
+            <Badge variant="secondary">In-Progress</Badge>
+          </h1>
+          <Ticket_List_PM_In_Progress
+              ticketsInProgress = {ticketsInProgress}
+              selectedProperty= {state_PM_Tickets.selectedProperty}
+              assignEmployeeToTicket = {assignEmployeeToTicket}
           />
         </div>
       </>
