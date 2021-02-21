499acb808a41ecf3edbdbcaf09d0e543f8454ca6
Amphakarn <pisuthigomol@yahoo.com>
2021-02-21 22:26:57 +0000

    Added additional fields to Ticket_List_PM_Pending and In_Progress

--- client/src/components/Dashboard_PM_Tickets/Ticket_List_Item_PM.js
+++ client/src/components/Dashboard_PM_Tickets/Ticket_List_Item_PM.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import Button from "./Button";
 // import Ticket_List_PM from "./Ticket_List_PM_Pending";
 import Employee_List_PM from "./Employee_List_PM";
-import MyVerticallyCenteredModal from './specificTicket_Modal';
+import MyVerticallyCenteredModal from "./specificTicket_Modal";
 import "./Ticket_List_Item_PM.scss";
 
 export default function Ticket_List_Item_PM(props) {
   const [modalShow, setModalShow] = React.useState(false);
 
+  console.log("***Ticket_List_Item_PM --props: ", props);
   const {
     ticketID,
     ticketStatus,
@@ -18,20 +19,22 @@ export default function Ticket_List_Item_PM(props) {
     maintenanceType,
     description,
   } = props;
+
+  console.log("***Ticket_List_Item_PM -- props: ", props);
   const hideModal = function (ticket_id, employee_id) {
     setModalShow(false);
     assignEmployeeToTicket(ticket_id, employee_id);
-    
-    
-  }
+  };
+
   const [ticketItemState, setState] = useState("employee-unassigned");
-  const {assignEmployeeToTicket} = props;
+  const { assignEmployeeToTicket } = props;
+
   return (
     <main className="ticket__cards">
       <section className="ticket__card-left">
-        <div>{unit}</div>
-        <div>{maintenanceType}</div>
-        <div>{description}</div>
+        {/* <div>{unit}</div> */}
+        <div>Maintenance type: {maintenanceType}</div>
+        <div>Description: {description}</div>
       </section>
       {/*
       <section className="ticket__validation">
@@ -40,25 +43,25 @@ export default function Ticket_List_Item_PM(props) {
       */}
       <section className="ticket__card-right">
         <div>{ticketStatus}</div>
-        <div>{ticketID}</div>
-        <div>{createdAt}</div>
+        <div>Ticket ID: {ticketID}</div>
+        <div>Created at: {createdAt}</div>
       </section>
       <>
         {
-        <Button variant="primary" onClick={() => setModalShow(true)}>
-          Assign Employee
-        </Button>
-      }
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            Assign Employee
+          </Button>
+        }
         <MyVerticallyCenteredModal
           employeeList={employees}
-          description = {description}
+          description={description}
           show={modalShow}
-          onHide = {hideModal}
-          ticket_id = {ticketID}
-          assignEmployeeToTicket = {assignEmployeeToTicket}
+          onHide={hideModal}
+          ticket_id={ticketID}
+          assignEmployeeToTicket={assignEmployeeToTicket}
         />
       </>
-       {/*onHide={() => setModalShow(false)}*/}
+      {/*onHide={() => setModalShow(false)}*/}
       <section className="ticket__actions">
         {/* <Button danger onClick={cancel}>
           Cancel
