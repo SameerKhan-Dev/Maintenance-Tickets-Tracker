a3d791db7139c6ddfb7ecad793b80ab159dcddc6
SameerKhan-Dev <sameerk_786@hotmail.com>
2021-02-24 00:10:57 +0000

    fixed navbar to the top of the page for dashboard-pm pages

--- client/src/components/Dashboard_PM_Stats/Top_Nav_Bar_PM_Stats.js
+++ client/src/components/Dashboard_PM_Stats/Top_Nav_Bar_PM_Stats.js
@@ -26,7 +26,7 @@ export default function Top_NavBar_PM_Stats(props) {
   );
   return (
     <div classname="App">
-      <Navbar collapseOnSelect expand="lg" bg='dark' variant="dark">
+      <Navbar className= "fixed-top" collapseOnSelect expand="lg" bg='dark' variant="dark">
       <Navbar.Brand href="/dashboard-pm-stats">
       <img
         alt=""
