c7a6fe748a0fcf3522c180a9721d1627828b154c
Amphakarn <pisuthigomol@yahoo.com>
2021-02-24 02:22:58 +0000

    Fixing seed data

--- back-end/db/seeds/01_users.sql
+++ back-end/db/seeds/01_users.sql
@@ -12,30 +12,34 @@
 INSERT INTO "users" (firstname, lastname, email, password, role_id, start_date)
 VALUES
     
-    ('Drew','Mills','Pink.Wunsch@yahoo.com','password',1,'2015-04-14T03:50:56.268Z'),    /* PM */
-    ('Frida','Padberg','frida_padberg.com','password',1,'2015-09-22T08:41:20.700Z'), /* PM */
-    ('Maci','Lehner','maci_lehner@gmail.com','password',1,'2015-09-22T08:41:20.700Z'),   /*Pm  - USE THIS*/ 
-    ('Celia','Kuvalis','Aiyana.Stracke@hotmail.com','password',2,'2015-10-13T17:58:48.011Z'), /* Tenant */
-    ('Carrie','Kessler','Selena_Hane@gmail.com','password',2,'2018-09-10T09:30:40.397Z'), /* Tenant */
-    ('Reva','Hilll','Sincere70@yahoo.com','password',2,'2017-12-18T01:16:04.626Z'), /* Tenant */
-    ('Shayne','Lindgren','Jaron.Greenholt45@yahoo.com','password',2,'2018-12-23T12:02:33.829Z'), /* Tenant */
-    ('Opal','Adams','Pearl_Spencer@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant */
-    ('Mike','James','Mike_James@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant */
-    ('Bryant','Vince','Bryant_Vince@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant */
-    ('Kobe','Pearl','Kobe_Pearl@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant */
-    ('Matthew','Harden','Matthew_Harden@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant */
-    ('Jack','Harvey','Jack_Harvey@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant */
-    ('Carl','Harvey','car_harvey@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant  - OPTION*/
-    ('Davey','Harvey','davey_harvey@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant - OPTION */
-    ('James','Harvey','james_harvey@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant  - OPTION */
-    ('Julio','Harvey','Benton35@gmail.com','password',3,'2019-01-10T22:45:05.783Z'), /* plumber */
-    ('Rylan','O''Conner','Gerardo.Krajcik57@hotmail.com','password',3,'2017-07-04T03:13:44.223Z'), /* plumber */
-    ('Rebecca','Schumm','Victoria98@yahoo.com','password',4,'2019-01-01T00:25:07.113Z'), /* electrician */
-    ('Barrett','Lehner','Teresa_Monahan@hotmail.com','password',4,'2016-04-04T16:36:32.413Z'), /* electrician */
-    ('Keon','Ankunding','Dorcas_Kulas@yahoo.com','password',5,'2016-09-08T05:42:28.509Z'), /* general-maintenance */
-    ('Reynold','Boyer','Harold64@hotmail.com','password',5,'2018-06-18T13:28:53.282Z'),  /* general-maintenance */
-    ('Mikel','Ruecker','Abe_Goodwin28@gmail.com','password',5,'2017-11-08T20:19:29.374Z'), /* general-maintenance */
-    ('Tim','Ankunding','tim_ankunding.com','password',3,'2016-09-08T05:42:28.509Z'), /* OPTION general-maintenance */
-    ('Mike','Boyer','mike_boyer@hotmail.com','password',4,'2018-06-18T13:28:53.282Z'),  /* OPTION general-maintenance */
-    ('Roger','Ruecker','roger_ruecker@gmail.com','password',4,'2017-11-08T20:19:29.374Z'); /* OPTION general-maintenance */
+    ('Drew','Mills','Pink.Wunsch@yahoo.com','password',1,'2015-04-14T03:50:56.268Z'), /* PM  1*/ 
+    ('Frida','Padberg','frida_padberg.com','password',1,'2015-09-22T08:41:20.700Z'), /* PM   2*/
+    ('Zahra', 'Mohamed', 'zahra_m@email.com', 'password',1,'2016-09-15T08:41:20.700Z'), /* PM - USE THIS 3*/ 
+    ('Celia','Kuvalis','Aiyana.Stracke@hotmail.com','password',2,'2015-10-13T17:58:48.011Z'), /* Tenant  4*/
+    ('Carrie','Kessler','Selena_Hane@gmail.com','password',2,'2018-09-10T09:30:40.397Z'), /* Tenant      5*/
+    ('Reva','Hilll','Sincere70@yahoo.com','password',2,'2017-12-18T01:16:04.626Z'), /* Tenant            6*/
+    ('Shayne','Lindgren','Jaron.Greenholt45@yahoo.com','password',2,'2018-12-23T12:02:33.829Z'), /* Tenant  7*/
+    ('Opal','Adams','Pearl_Spencer@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant        8*/
+    ('Mike','James','Mike_James@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant           9*/
+    ('Bryant','Vince','Bryant_Vince@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant       10*/
+    ('Kobe','Pearl','Kobe_Pearl@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant           11*/
+    ('Matthew','Harden','Matthew_Harden@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant   12*/
+    ('Bee','Lister','bee_l@email.com','password',2,'2018-10-13T17:58:48.011Z'), /* Tenant - USE THIS     13*/
+    ('Carl','Harvey','car_harvey@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant - OPTION 14*/
+    ('Davey','Harvey','davey_harvey@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant - OPTION  15*/
+    ('James','Harvey','james_harvey@yahoo.com','password',2,'2016-06-12T08:03:52.569Z'), /* Tenant - OPTION  16*/
+    ('Julio','Harvey','Benton35@gmail.com','password',3,'2019-01-10T22:45:05.783Z'), /* plumber           17*/
+    ('Rylan','O''Conner','Gerardo.Krajcik57@hotmail.com','password',3,'2017-07-04T03:13:44.223Z'), /* plumber 18*/
+    ('Rebecca','Schumm','Victoria98@yahoo.com','password',4,'2019-01-01T00:25:07.113Z'), /* electrician   19*/
+    ('Barrett','Lehner','Teresa_Monahan@hotmail.com','password',4,'2016-04-04T16:36:32.413Z'), /* electrician 20*/
+    ('Keon','Ankunding','Dorcas_Kulas@yahoo.com','password',5,'2016-09-08T05:42:28.509Z'), /* general-maintenance 21*/
+    ('Reynold','Boyer','Harold64@hotmail.com','password',5,'2018-06-18T13:28:53.282Z'), /* general-maintenance    22*/
+    ('Mikel','Ruecker','Abe_Goodwin28@gmail.com','password',5,'2017-11-08T20:19:29.374Z'), /* general-maintenance 23*/
+    ('Sameer','Khan','sameer_k@email.com','password',3,'2017-07-04T03:13:44.223Z'), /* plumber - USE THIS         24*/
+    ('Mike','Boyer','mike_boyer@hotmail.com','password',4,'2018-06-18T13:28:53.282Z'), /* OPTION general-maintenance 25*/
+    ('Roger','Ruecker','roger_ruecker@gmail.com','password',4,'2017-11-08T20:19:29.374Z'); /* OPTION general-maintenance 26*/
+        
+
+
+    
     
