e2f3b55c0307e816d2e0ecc3031cc8fb1e90c146
Amphakarn <pisuthigomol@yahoo.com>
2021-02-28 20:54:03 +0000

    Added README and Screenshots

---
+++ README.md
@@ -0,0 +1,41 @@
+# M-Tracker
+
+M-Tracker or Maintenance Tracker is a team collaboration to build an end-to-end solution that helps property managers or landlords track the progress of maintenance issues for their properties. Starting from tenants submitting tickets, property managers assigning tasks to employees and employees entering cost of maintenance upon completion of tickets. The system also provides statistical data on maintenance issues and expenses which assists property mangers or owners with business forecast.
+
+## Getting started
+
+This application requires two servers running, the back-end server runs on port 8080 and the client runs on port 8000. From the root directory of the project, execute the following commands:
+
+1. Install dependencies with `npm install`.
+2. Once the installation is completed:
+   2.1) On the back-end terminal, run `npm run local`.
+   2.2) On the client terminal, run `npm start`.
+3. Visit http://localhost:8000/ in the browser
+
+## Dependencies
+
+• react
+• react-bootstrap
+• react-google-charts
+• react-bootstrap
+• react-dom
+• react-scripts
+• bootstrap
+• axios
+• mapbox-gl
+• Node.js
+• Express
+
+## Screenshots
+
+!["Login page"]()
+
+!["Dashboard Tenant"]()
+
+!["Dashboard PM Statistics"]()
+
+!["Dashboard PM Tickets"]()
+
+!["Dashboard Employee Summary"]()
+
+!["Dashboard Employee Ticket"]()
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Form, Button, Image } from 'react-bootstrap';
+import { Form, Button, Image } from "react-bootstrap";
 import "./Login.scss";
 // import background from "./Dashboard_Tenant/assets/background.jpg";
 import logo from "./MTrack_White.png";
@@ -39,17 +39,17 @@ export default function Login(props) {
   //  4 ('electrician'),
   //  5 ('general-maintenance')
 
-  const getPathForRole = ((userRole) => {
+  const getPathForRole = (userRole) => {
     const paths = {
       1: "/dashboard-pm-stats",
       2: "/dashboard-tenant",
       3: "/dashboard-employee",
       4: "/dashboard-employee",
-      5: "/dashboard-employee"
-    }
+      5: "/dashboard-employee",
+    };
 
     return paths[userRole] || "/login";
-  });
+  };
   // *******************
   const onLoginSubmit = () => {
     const currentEmail = inputsState.emailInput;
@@ -70,7 +70,7 @@ export default function Login(props) {
             ...prev,
             loggedIn: true,
             userEmail: response.data.email,
-            userRole: response.data.role_id
+            userRole: response.data.role_id,
           }));
           history.push(getPathForRole(response.data.role_id));
         }
@@ -94,34 +94,43 @@ export default function Login(props) {
             <h1 className="login__title">Login Here!</h1>
             <Form.Group controlId="formGroupEmail">
               <Form.Label className="form__label">Email address</Form.Label>
-              <Form.Control className="issue__create-input"
+              <Form.Control
+                className="issue__create-input"
                 value={inputsState.emailInput}
                 name="email"
-                onChange={(event) =>
-                  onFormChange("email", event.target.value)
-                }
+                onChange={(event) => onFormChange("email", event.target.value)}
                 description="description"
                 type="email"
-                placeholder="Enter email..." />
+                placeholder="Enter email..."
+              />
             </Form.Group>
             <Form.Group controlId="formGroupPassword">
               <Form.Label className="form__label">Password</Form.Label>
-              <Form.Control type="password"
+              <Form.Control
+                type="password"
                 name="password"
                 placeholder="Enter password..."
                 value={inputsState.passwordInput}
                 onChange={(event) =>
                   onFormChange("password", event.target.value)
-                } />
+                }
+              />
             </Form.Group>
             <h5 className="no__account">No account?</h5>
-            <h5 className="no__account">Click<a href="/register"> here</a> to register!</h5>
-            <Button className="issue__actions" onClick={onLoginSubmit} variant="secondary" type="submit">
-              Empty Submit
+            <h5 className="no__account">
+              Click<a href="/register"> here</a> to register!
+            </h5>
+            <Button
+              className="issue__actions"
+              onClick={onLoginSubmit}
+              variant="secondary"
+              type="submit"
+            >
+              Login
             </Button>
           </Form>
         </section>
       </section>
     </>
-  )
+  );
 }
