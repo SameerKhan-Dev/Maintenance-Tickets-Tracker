dce37705ae26fd4f02bc9926256ba5093df698dd
Amphakarn <pisuthigomol@yahoo.com>
2021-03-08 15:58:45 +0000

    Changed property register form size and added upload image form field

--- client/src/components/Register_Property.js
+++ client/src/components/Register_Property.js
@@ -6,6 +6,18 @@ import background from "./tools__logo.jpg";
 import "./Register_Property.scss";
 
 export default function Register_Property(props) {
+  const [inputsState, setInputsState] = useState({
+    pmId: "",
+    propertyType: "",
+    propertyName: "",
+    address: "",
+    unit: "",
+    city: "",
+    province: "",
+    postalCode: "",
+    imagePath: "",
+  });
+
   return (
     <>
       <div className="background-img">
@@ -31,41 +43,51 @@ export default function Register_Property(props) {
         </InputGroup> */}
 
         <h5>Enter Property Information:</h5>
+
         <Form inSubmit={(event) => event.preventDefault()}>
           <Form.Group controlId="exampleForm.ControlSelect1">
             <Form.Label className="text__property-type">
               Select property type:
             </Form.Label>
-            <Form.Control as="select">
+            <Form.Control as="select" size="sm">
               <option>Condominium/Apartment Building</option>
               <option>Individual Home</option>
             </Form.Control>
           </Form.Group>
 
-          <InputGroup className="mb-3">
+          <InputGroup className="mb-3" size="sm">
             <InputGroup.Prepend>
               <InputGroup.Text id="inputGroup-sizing-default">
                 Property Name
               </InputGroup.Text>
             </InputGroup.Prepend>
             <FormControl
+              value={inputsState.propertyName}
+              name="propertyName"
+              // onChange={(event) =>
+              //   onFormChange("propertyName", event.target.value)
+              // }
+              type="text"
+              placeholder="Enter property name"
               aria-label="Default"
               aria-describedby="inputGroup-sizing-default"
             />
           </InputGroup>
 
-          <InputGroup className="mb-3">
+          <InputGroup size="sm">
             <InputGroup.Prepend>
-              <InputGroup.Text id="inputGroup-sizing-default">
+              <InputGroup.Text id="inputGroup-sizing-default" className="mb-3">
                 Address
               </InputGroup.Text>
-              <FormControl className="mr-4"
-              // aria-label="Default"
-              // aria-describedby="inputGroup-sizing-default"
+              <FormControl
+                className="mr-3"
+                size="sm"
+                // aria-label="Default"
+                // aria-describedby="inputGroup-sizing-default"
               />
             </InputGroup.Prepend>
             <InputGroup.Prepend>
-              <InputGroup.Text id="inputGroup-sizing-default">
+              <InputGroup.Text id="inputGroup-sizing-default" className="mb-3">
                 Unit#
               </InputGroup.Text>
             </InputGroup.Prepend>
@@ -87,7 +109,7 @@ export default function Register_Property(props) {
             />
           </InputGroup> */}
 
-          <InputGroup className="mb-3">
+          <InputGroup className="mb-3" size="sm">
             <InputGroup.Prepend>
               <InputGroup.Text>City</InputGroup.Text>
               {/* <FormControl /> */}
@@ -98,7 +120,7 @@ export default function Register_Property(props) {
             />
           </InputGroup>
 
-          <InputGroup className="mb-3">
+          <InputGroup className="mb-3" size="sm">
             <InputGroup.Prepend>
               <InputGroup.Text className="text__prov-state">
                 Province/State
@@ -111,7 +133,7 @@ export default function Register_Property(props) {
             />
           </InputGroup>
 
-          <InputGroup className="mb-3">
+          <InputGroup className="mb-3" size="sm">
             <InputGroup.Prepend>
               <InputGroup.Text id="inputGroup-sizing-default">
                 Postal Code
@@ -123,11 +145,39 @@ export default function Register_Property(props) {
             />
           </InputGroup>
 
+          <Form className="mb-3">
+            <Form.File
+              size="sm"
+              id="chooseImageUpload"
+              label="Choose image upload"
+              custom
+            />
+          </Form>
+
+          {/* <div className="input-group mb-3">
+            <div className="custom-file">
+              <input
+                type="file"
+                className="custom-file-input"
+                id="chooseImageUpload"
+              ></input>
+              <label className="custom-file-label" for="chooseImageUpload">
+                Choose file
+              </label>
+            </div>
+            <div className="input-group-append">
+              <button className="btn btn-secondary" type="button">
+                Upload
+              </button>
+            </div>
+          </div> */}
+
           <Button
             className="float-right"
             // onClick={onPropertyRegisterSubmit}
             variant="secondary"
             type="submit"
+            size="sm"
           >
             Save Property Information
           </Button>
